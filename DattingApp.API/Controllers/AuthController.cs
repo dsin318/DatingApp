93d81ac0aa9c94c012b57511489f4956a898d6ef
unknown <dsin318@gmail.com>
2019-10-12 18:37:06 -0400

    end of section 6

--- DattingApp.API/Controllers/AuthController.cs
+++ DattingApp.API/Controllers/AuthController.cs
@@ -51,7 +51,7 @@ namespace DattingApp.API.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
-           throw new System.Exception("Computer says NO");
+          // throw new System.Exception("Computer says NO");
 
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
